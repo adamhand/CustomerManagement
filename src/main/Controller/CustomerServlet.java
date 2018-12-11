69217bcf778991d6d40c3bf1a1e3947f06248c55
adamhand <adaihand@163.com>
2018-12-11 12:57:41 +0800

    add some methods in CustomerServlet and PageBean

--- src/main/Controller/CustomerServlet.java
+++ src/main/Controller/CustomerServlet.java
@@ -3,6 +3,7 @@ package Controller;
 import Utils.BaseServlet;
 import Utils.WebUtils;
 import domain.Customer;
+import domain.PageBean;
 import service.CustomerService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +20,66 @@ public class CustomerServlet extends BaseServlet{
         cs.addCustomer(customer);
 
         request.setAttribute("msg", "恭喜，成功添加客户！");
+
+        return "";
+    }
+
+    public String preEdit(HttpServletRequest request, HttpServletResponse response){
+        String id = request.getParameter("id");
+        Customer customer = cs.findCustomerById(id);
+
+        request.setAttribute("customer", customer);
+
+        return "";
+    }
+
+    public String editCustomer(HttpServletRequest request, HttpServletResponse response){
+        Customer customer = WebUtils.fillBean(request, Customer.class);
+        cs.editCustomer(customer);
+
+        request.setAttribute("msg", "恭喜，修改客户信息成功！");
+
+        return "";
+    }
+
+    public String deleteCustomer(HttpServletRequest request, HttpServletResponse response){
+        String id = request.getParameter("id");
+        cs.deleteCustomerById(id);
+
+        request.setAttribute("msg", "恭喜，删除客户信息成功！");
+
+        return "";
+    }
+
+    private int getPc(HttpServletRequest request){
+        String value = request.getParameter("pc");
+        if(value == null || value.trim().isEmpty())
+            return 1;
+
+        return Integer.parseInt(value);
+    }
+
+    private String getUrl(HttpServletRequest request) {
+        String contextPath = request.getContextPath();
+        String servletPath = request.getServletPath();
+        String queryString = request.getQueryString();
+
+        if (queryString.contains("&pc=")) {
+            int index = queryString.lastIndexOf("&pc=");
+            queryString = queryString.substring(0, index);
+        }
+
+        return contextPath + servletPath + "?" + queryString;
+    }
+
+    public String findAll(HttpServletRequest request, HttpServletResponse response){
+        int pc = getPc(request);
+        int pr = 10;
+
+        PageBean<Customer> pb = cs.findAll(pc, pr);
+        pb.setUrl(getUrl(request));
+
+        request.setAttribute("pb", pb);
 
         return "";
     }
--- src/main/domain/PageBean.java
+++ src/main/domain/PageBean.java
@@ -11,5 +11,5 @@ public class PageBean<Object> {
     private int totalRecords;          //总纪录数tatal records
     private int pageRecords;          //每页纪录数page records
     private List<Object> beanList;//当前页的纪录
-    private String url;
+    private String url;           //当前用户的url
 }
